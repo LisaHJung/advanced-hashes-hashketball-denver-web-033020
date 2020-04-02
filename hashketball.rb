b5ef240538dea72245971c28a51afb6523205370
Lisa Jung <lisahjung@gmail.com>
2020-04-02 21:43:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,7 @@
 require "pry"
 require "pp"
 
+
 def game_hash 
   {
   :home => {:team_name =>"Brooklyn Nets",
@@ -62,6 +63,7 @@ def game_hash
             }
           ]
         },
+<<<<<<< HEAD
 
   :away => {:team_name =>"Charlotte Hornets",
             :colors=>["Turquoise", "Purple"],
@@ -125,6 +127,7 @@ end
   
 def  num_points_scored (players_name)
   game_hash.each do |place, team|
+    binding.pry
     team.each do |team_info_key, team_info_value|
     if team_info_key == :players
       team_info_value.each do |player_info|
@@ -235,10 +238,164 @@ def most_points_scored
       end
     end
     player_name
+=======
+
+  :away => {:team_name =>"Charlotte Hornets",
+            :colors=>["Turquoise", "Purple"],
+            :players =>[ 
+            {:player_name => "Jeff Adrien",
+              :number => 4,
+              :shoe => 18,
+              :points => 10,
+              :rebounds => 1,
+              :assists => 1,
+              :steals => 2,
+              :blocks => 7,
+              :slam_dunks => 2,
+            },
+            
+             {:player_name => "Bismack Biyombo",
+              :number => 0,
+              :shoe => 16,
+              :points => 12,
+              :rebounds => 4,
+              :assists => 7,
+              :steals => 22,
+              :blocks => 15,
+              :slam_dunks => 10,
+            },
+            
+             {:player_name => "DeSagna Diop",
+              :number => 2,
+              :shoe => 14,
+              :points => 24,
+              :rebounds => 12,
+              :assists => 12,
+              :steals => 4,
+              :blocks => 5,
+              :slam_dunks => 5,
+            },
+             {:player_name => "Ben Gordon",
+              :number => 8,
+              :shoe => 15,
+              :points => 33,
+              :rebounds => 3,
+              :assists => 2,
+              :steals => 1,
+              :blocks => 1,
+              :slam_dunks => 0,
+            },
+             {:player_name => "Kemba Walker",
+              :number => 33,
+              :shoe => 15,
+              :points => 6,
+              :rebounds => 12,
+              :assists => 12,
+              :steals => 7,
+              :blocks => 5,
+              :slam_dunks => 12,
+            }
+          ]
+         }
+}
+end
+  
+def  num_points_scored (players_name)
+  game_hash.each do |place, team|
+    team.each do |team_info_key, team_info_value|
+    if team_info_key == :players
+      team_info_value.each do |player_info|
+        if player_info[:player_name] == players_name
+          return player_info[:points]
+        end
+      end
+    end   
+   end    
+ end
+end
+  
+def  shoe_size (players_name)
+  game_hash.each do |place, team|
+    team.each do |team_info_key, team_info_value|
+    if team_info_key == :players
+      team_info_value.each do |player_info|
+        if player_info[:player_name] == players_name
+          return player_info[:shoe]
+        end
+      end
+    end   
+   end    
+ end
+>>>>>>> b6486abaaec8444d75e7a06740deb46afb7548a0
 end
 
+def team_colors(team_name)
+  game_hash.each do|place, team|
+    if team[:team_name] == team_name
+      return team[:colors]
+     end
+ end
+end
+    
+def team_names
+  new_arr= []
+  game_hash.each do|place, team|
+    new_arr<< team[:team_name]
+  end
+  new_arr
+end
 
+def player_numbers(team_name)
+  new_array =[]
+  game_hash.each do |place, team|
+    if team[:team_name] == team_name
+      team.each do |team_info_key, team_info_value|
+        if team_info_key == :players
+          team_info_value.each do |player_info|
+            new_array<< player_info[:number]
+           end
+         end
+       end
+     end
+   end
+   new_array
+end
 
+def player_stats(player_name)
+  new_hash= {}
+   game_hash.each do |place, team|
+      team.each do |team_info_key, team_info_value|
+        if team_info_key == :players
+          team_info_value.each do |player|
+            if player[:player_name] == player_name
+             new_hash = player.delete_if do |k,v|
+               k==:player_name
+             end
+         end
+       end
+     end
+   end
+ end
+ new_hash
+end
+  
+def big_shoe_rebounds
+  biggest_shoe =0
+  rebounds =0
+ game_hash.each do |place, team|
+      team.each do |team_info_key, team_info_value|
+        if team_info_key == :players
+          team_info_value.each do |player|
+            if player[:shoe] > biggest_shoe
+              biggest_shoe = player[:shoe]
+              rebounds = player[:rebounds]
+            end
+          end
+        end
+      end
+    end
+    rebounds
+end
 
 
 =begin
